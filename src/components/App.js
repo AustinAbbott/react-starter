0459989d32af434017c0bd76244fc4cc3bedb3a5
Austin Abbott <magicaustin51547@gmail.com>
2020-02-20 19:09:21 -0700

    Solve level 0 and part of level 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import "../main.css";
+import movies from "./Movies.js"
 
 class App extends React.Component {
-  render(){
-    return(
-    <div>
-      Starter App component - hello there
-    </div>
-  )}
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true, movieList: movies
+
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log('It worked')
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+
+  }
+
+  render() {
+    return (
+      <div className="Container">
+        <header><h1>MovieList</h1></header>
+
+        <form>
+          <input type="text"></input>
+          <button onClick={this.handleClick}>Search</button>
+        </form>
+
+        {this.state.movieList.map(eachMovie => (
+          <div>{eachMovie.title}</div>
+        ))}
+
+      </div>
+    )
+  }
 }
 
 export default App;
